5f9bd6dfa9a121ea9ef07d9b277a95e8706b6d7d
wahyuptama <wahyulucky28@gmail.com>
2020-07-03 13:32:47 +0700

    initial commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Movie App</title>
+
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+<body class="font-sans bg-gray-900 text-white">
+    <nav class="border-b border-gray-800">
+        <div class="container mx-auto">
+            <div class="container mx-auto flex items-center justify-between px-4 py-6">
+                <ul class="flex item-center">
+                    <li>
+                        <a href="#">
+                            
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    @yield('content')
+</body>
+</html>
